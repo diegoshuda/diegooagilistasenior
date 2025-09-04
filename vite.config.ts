f65e632a0eb3d5223bbbea32b084d72568fd4507
diegoshuda <78032100+diegoshuda@users.noreply.github.com>
2025-09-04 09:54:06 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+base: '/diegoagilista/
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
