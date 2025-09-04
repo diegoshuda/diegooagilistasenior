aac05743aebcbb74d3bd077d2c560f4f433e94bd
diegoshuda <78032100+diegoshuda@users.noreply.github.com>
2025-09-04 10:39:00 -0300

    Updssate vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -13,5 +13,8 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '/diegooagilista/'
-}); 
+  base: '/diegooagilista/',
+  build: {
+    outDir: 'dist'
+  }
+});
